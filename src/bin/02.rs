6d8cc41ac3f361f1872d3cc2c00d557b89f6aedf
JGDoerrer <jg.doerrer@gmail.com>
2025-12-02 17:25:06 +0100

    optimize day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -10,17 +10,26 @@ pub fn part_one(input: &str) -> Option<u64> {
         })
         .collect();
 
+    const POWERS: [u64; 10] = [
+        1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000,
+    ];
+
     let mut total = 0;
     for (min, max) in ranges {
-        for num in min..=max {
-            let str = num.to_string();
-
-            if str.len() % 2 != 0 {
+        let mut num = min;
+        while num < max {
+            let digits = num.ilog10() + 1;
+            if digits % 2 != 0 {
+                num = POWERS[digits as usize];
                 continue;
             }
-            if str == format!("{}{0}", &str[0..str.len() / 2]) {
+
+            let half = POWERS[digits as usize / 2];
+
+            if num / half == num % half {
                 total += num;
             }
+            num += 1;
         }
     }
 
@@ -37,17 +46,34 @@ pub fn part_two(input: &str) -> Option<u64> {
         })
         .collect();
 
+    const POWERS: [u64; 10] = [
+        1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000,
+    ];
+
     let mut total = 0;
     for (min, max) in ranges {
         for num in min..=max {
-            let str = num.to_string();
+            let digits = num.ilog10() + 1;
 
-            for i in 1..str.len() {
-                let repeat = str[0..i].repeat(str.len() / i);
-                if str == repeat {
-                    total += num;
-                    break;
+            'i: for i in 1..digits {
+                if digits % i != 0 {
+                    continue;
                 }
+
+                let pow = POWERS[i as usize];
+                let last_digits = num % pow;
+                let mut rest = num;
+
+                for _ in 0..digits / i {
+                    if rest % pow != last_digits {
+                        continue 'i;
+                    }
+
+                    rest /= pow;
+                }
+
+                total += num;
+                break;
             }
         }
     }
